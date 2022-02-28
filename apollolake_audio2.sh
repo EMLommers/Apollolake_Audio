676e847895bc525dd417973bee9e9b8039a93cae
Accumulator <79648274+EMLommers@users.noreply.github.com>
2022-02-28 15:26:25 +0100

    Add files via upload

---
+++ apollolake_audio2.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+sudo apt install alsa-topology* alsa-ucm* linux-firmware
+sudo cp sof-apl-da7219.tplg /lib/firmware/intel/sof-tplg/
+sudo sh -c 'echo "options snd_intel_dspcfg dsp_driver=3" > /etc/modprobe.d/inteldsp.conf' 
+reboot
+
